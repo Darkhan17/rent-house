76bb6318b5d2df287330454fd3bb8027785e2b86
Darkhan Khamitov <dkhamitov@dar.kz>
2023-05-08 23:48:02 +0600

    fix

--- target/generated-sources/annotations/kz/kbtu/renthouse/mapper/HouseMapperImpl.java
+++ target/generated-sources/annotations/kz/kbtu/renthouse/mapper/HouseMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-08T23:38:17+0600",
+    date = "2023-05-08T23:45:52+0600",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
