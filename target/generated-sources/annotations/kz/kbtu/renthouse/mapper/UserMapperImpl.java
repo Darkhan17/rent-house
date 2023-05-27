1e69cb9e58ce3f6cae38a4f649b95487b79888ca
Darkhan Khamitov <dkhamitov@dar.kz>
2023-05-27 22:10:10 +0600

    configs

--- target/generated-sources/annotations/kz/kbtu/renthouse/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/kz/kbtu/renthouse/mapper/UserMapperImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-27T21:37:02+0600",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Homebrew)"
+    date = "2023-05-27T22:09:24+0600",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
